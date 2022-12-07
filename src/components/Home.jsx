4b36c2afd71d4f72f69408872dc9defcdea08b62
br <brunno00amorim@gmail.com>
2022-12-07 00:27:48 -0300

    to deploy :octocat:

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,64 @@
 
 import React from "react";
 import { Input, Box, Button, Heading} from '@chakra-ui/react'
+import {useState} from 'react'
+import Task from "./Task";
 
 export function Home (props){
 
 
+    const [task, setTask] = useState("");
+
+    const [arrayTask, setArrayTask] = useState([])
+
+    
+    const handleTask = (event) => {
+        event.preventDefault()
+        setTask(event.target.value)
+        console.log(event.target.value)
+      }
+       
+    
+    
+    const createTask = () => {
+    const idTask = Date.now();
+
+        const newTask = {
+            tarefa: task,
+            idTask
+        } 
+        const arrayToSpread = [...arrayTask, newTask]
+        setArrayTask(arrayToSpread)
+        setTask("")
+        console.table(arrayTask)
+    }
+
+    function deleteTask (id){
+        const filterToDelete = arrayTask.filter((task)=>{
+           if(id!== task.idTask){
+               return(task)
+           }      
+    })
+    setArrayTask(filterToDelete)
+    }
+    
+
+    const taskMapeado = arrayTask.map((task) => {
+        return (
+            <Task key={task.idTask} tarefa={task} deleteTask={deleteTask} ></Task>
+        )
+    })
+
+    
     return(<div>
 
         <Heading m={5}>Lista de tarefas</Heading>
             <Box w={'30%'} m={5} >
-            <Input placeholder='Qual sua próxima tarefa?'  />
-                <Button colorScheme='teal' size='sm' mt='2' w='30%'>
+            <Input placeholder='Qual sua próxima tarefa?' onChange={handleTask} value={task} />
+                <Button colorScheme='teal' size='sm' mt='2' w='100%' onClick={createTask}>
                 Enviar
             </Button>
+            {taskMapeado}
             </Box>
  
     </div>)
---
+++ src/components/Task.jsx
@@ -0,0 +1,14 @@
+import { Box, Button } from '@chakra-ui/react'
+function Task(props) {
+//Componente recebendo valor do elemento pai (props)
+//observem: props.valor (mesmo nome do valor enviado pelo elemento pai)
+    const { tarefa, idTask } = props.tarefa
+    console.log(tarefa, idTask)
+    return (
+        <Box w='100%' mt={"20px"}>
+            <Button w='100%' onClick={()=>props.deleteTask(idTask)}>
+                {tarefa}
+            </Button>
+        </Box>)
+}
+export default Task
